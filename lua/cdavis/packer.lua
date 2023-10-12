e9b0af6ce6799254500286359cf72362754f27e3
Davis, Christian <Christian_Davis@comcast.com>
2023-10-12 11:39:09 -0400

    bang bang

---
+++ lua/cdavis/packer.lua
@@ -0,0 +1,46 @@
+vim.cmd [[packadd packer.nvim]]
+
+return require('packer').startup(function(use)
+  -- Packer can manage itself
+  use 'wbthomason/packer.nvim'
+
+  use {
+	  'nvim-telescope/telescope.nvim', tag = '0.1.3',
+	  -- or                            , branch = '0.1.x',
+	  requires = { {'nvim-lua/plenary.nvim'} }
+  }
+
+  use({
+	  'sainnhe/gruvbox-material',
+	  config = function()
+		  vim.cmd('colorscheme gruvbox-material')
+	  end
+  })
+  --[[
+  rebelot/kanagawa.nvim
+  kanagawa-wave  kanagawa-dragon kanagawa-lotus
+  rose-pine/neovim
+  sainnhe/everforest  gruvbox-material
+  morhetz/gruvbox  ellisonleo/gruvbox.nvim
+  ]]--
+  use('nvim-treesitter/nvim-treesitter', {run = ':TSUpdate'})
+  use('nvim-treesitter/playground')
+  use('mbbill/undotree')
+  use('tpope/vim-fugitive')
+
+  use {
+  'VonHeikemen/lsp-zero.nvim',
+  branch = 'v3.x',
+  requires = {
+    -- LSP Support
+    {'neovim/nvim-lspconfig'},             -- Required
+    {'williamboman/mason.nvim'},           -- Optional
+    {'williamboman/mason-lspconfig.nvim'}, -- Optional
+
+    -- Autocompletion
+    {'hrsh7th/nvim-cmp'},     -- Required
+    {'hrsh7th/cmp-nvim-lsp'}, -- Required
+    {'L3MON4D3/LuaSnip'},     -- Required
+  }
+}
+end)
