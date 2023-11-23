925c649da6578ef39b99b18cbdf3e9b694c9c545
Davis, Christian <Christian_Davis@comcast.com>
2023-11-22 22:25:20 -0500

    update

--- lua/cdavis/packer.lua
+++ lua/cdavis/packer.lua
@@ -1,47 +1,67 @@
 vim.cmd [[packadd packer.nvim]]
 
 return require('packer').startup(function(use)
-  -- Packer can manage itself
-  use 'wbthomason/packer.nvim'
+    -- Packer can manage itself
+    use 'wbthomason/packer.nvim'
 
-  use {
-	  'nvim-telescope/telescope.nvim', tag = '0.1.3',
-	  -- or                            , branch = '0.1.x',
-	  requires = { {'nvim-lua/plenary.nvim'} }
-  }
+    use {
+        'nvim-telescope/telescope.nvim', tag = '0.1.4',
+        -- or                            , branch = '0.1.x',
+        requires = { {'nvim-lua/plenary.nvim'} }
+    }
 
-  use({
-	  'sainnhe/gruvbox-material',
-	  config = function()
-		  vim.cmd('colorscheme gruvbox-material')
-	  end
+    use({
+        'sainnhe/gruvbox-material',
+        config = function()
+            vim.cmd('colorscheme gruvbox-material')
+        end
+    })
+    --[[
+    rebelot/kanagawa.nvim
+    kanagawa-wave  kanagawa-dragon kanagawa-lotus
+    rose-pine/neovim
+    sainnhe/everforest  gruvbox-material
+    morhetz/gruvbox  ellisonleo/gruvbox.nvim
+    ]]--
+    use({
+      "folke/trouble.nvim",
+      config = function()
+          require("trouble").setup {
+              icons = false,
+              -- your configuration comes here
+              -- or leave it empty to use the default settings
+              -- refer to the configuration section below
+          }
+      end
   })
-  --[[
-  rebelot/kanagawa.nvim
-  kanagawa-wave  kanagawa-dragon kanagawa-lotus
-  rose-pine/neovim
-  sainnhe/everforest  gruvbox-material
-  morhetz/gruvbox  ellisonleo/gruvbox.nvim
-  ]]--
-  use('nvim-treesitter/nvim-treesitter', {run = ':TSUpdate'})
-  use('nvim-treesitter/playground')
-  use('mbbill/undotree')
-  use('tpope/vim-fugitive')
+    use {
+        'nvim-treesitter/nvim-treesitter',
+        run = function()
+            local ts_update = require('nvim-treesitter.install').update({ with_sync = true })
+            ts_update()
+        end,}
+        use('nvim-treesitter/playground')
+        use('nvim-treesitter/nvim-treesitter-context')
+        use('theprimeagen/harpoon')
+        use('mbbill/undotree')
+        use('tpope/vim-fugitive')
+        use('github/copilot.vim')
+        use('laytan/cloak.nvim')
 
-  use {
-  'VonHeikemen/lsp-zero.nvim',
-  branch = 'v3.x',
+        use {
+            'VonHeikemen/lsp-zero.nvim',
+            branch = 'v3.x',
 
-  requires = {
-    -- LSP Support
-    {'neovim/nvim-lspconfig'},             -- Required
-    {'williamboman/mason.nvim'},           -- Optional
-    {'williamboman/mason-lspconfig.nvim'}, -- Optional
+            requires = {
+                -- LSP Support
+                {'neovim/nvim-lspconfig'},             -- Required
+                {'williamboman/mason.nvim'},           -- Optional
+                {'williamboman/mason-lspconfig.nvim'}, -- Optional
 
-    -- Autocompletion
-    {'hrsh7th/nvim-cmp'},     -- Required
-    {'hrsh7th/cmp-nvim-lsp'}, -- Required
-    {'L3MON4D3/LuaSnip'},     -- Required
-  }
-}
-end)
+                -- Autocompletion
+                {'hrsh7th/nvim-cmp'},     -- Required
+                {'hrsh7th/cmp-nvim-lsp'}, -- Required
+                {'L3MON4D3/LuaSnip'},     -- Required
+            }
+        }
+    end)
--- plugin/packer_compiled.lua
+++ plugin/packer_compiled.lua
@@ -79,16 +79,31 @@ _G.packer_plugins = {
     path = "/Users/cdavis278/.local/share/nvim/site/pack/packer/start/LuaSnip",
     url = "https://github.com/L3MON4D3/LuaSnip"
   },
+  ["cloak.nvim"] = {
+    loaded = true,
+    path = "/Users/cdavis278/.local/share/nvim/site/pack/packer/start/cloak.nvim",
+    url = "https://github.com/laytan/cloak.nvim"
+  },
   ["cmp-nvim-lsp"] = {
     loaded = true,
     path = "/Users/cdavis278/.local/share/nvim/site/pack/packer/start/cmp-nvim-lsp",
     url = "https://github.com/hrsh7th/cmp-nvim-lsp"
+  },
+  ["copilot.vim"] = {
+    loaded = true,
+    path = "/Users/cdavis278/.local/share/nvim/site/pack/packer/start/copilot.vim",
+    url = "https://github.com/github/copilot.vim"
   },
   ["gruvbox-material"] = {
     config = { "\27LJ\2\n@\0\0\3\0\3\0\0056\0\0\0009\0\1\0'\2\2\0B\0\2\1K\0\1\0!colorscheme gruvbox-material\bcmd\bvim\0" },
     loaded = true,
     path = "/Users/cdavis278/.local/share/nvim/site/pack/packer/start/gruvbox-material",
     url = "https://github.com/sainnhe/gruvbox-material"
+  },
+  harpoon = {
+    loaded = true,
+    path = "/Users/cdavis278/.local/share/nvim/site/pack/packer/start/harpoon",
+    url = "https://github.com/theprimeagen/harpoon"
   },
   ["lsp-zero.nvim"] = {
     loaded = true,
@@ -120,6 +135,11 @@ _G.packer_plugins = {
     path = "/Users/cdavis278/.local/share/nvim/site/pack/packer/start/nvim-treesitter",
     url = "https://github.com/nvim-treesitter/nvim-treesitter"
   },
+  ["nvim-treesitter-context"] = {
+    loaded = true,
+    path = "/Users/cdavis278/.local/share/nvim/site/pack/packer/start/nvim-treesitter-context",
+    url = "https://github.com/nvim-treesitter/nvim-treesitter-context"
+  },
   ["packer.nvim"] = {
     loaded = true,
     path = "/Users/cdavis278/.local/share/nvim/site/pack/packer/start/packer.nvim",
@@ -140,6 +160,12 @@ _G.packer_plugins = {
     path = "/Users/cdavis278/.local/share/nvim/site/pack/packer/start/telescope.nvim",
     url = "https://github.com/nvim-telescope/telescope.nvim"
   },
+  ["trouble.nvim"] = {
+    config = { "\27LJ\2\nC\0\0\3\0\4\0\a6\0\0\0'\2\1\0B\0\2\0029\0\2\0005\2\3\0B\0\2\1K\0\1\0\1\0\1\nicons\1\nsetup\ftrouble\frequire\0" },
+    loaded = true,
+    path = "/Users/cdavis278/.local/share/nvim/site/pack/packer/start/trouble.nvim",
+    url = "https://github.com/folke/trouble.nvim"
+  },
   undotree = {
     loaded = true,
     path = "/Users/cdavis278/.local/share/nvim/site/pack/packer/start/undotree",
@@ -153,6 +179,10 @@ _G.packer_plugins = {
 }
 
 time([[Defining packer_plugins]], false)
+-- Config for: trouble.nvim
+time([[Config for trouble.nvim]], true)
+try_loadstring("\27LJ\2\nC\0\0\3\0\4\0\a6\0\0\0'\2\1\0B\0\2\0029\0\2\0005\2\3\0B\0\2\1K\0\1\0\1\0\1\nicons\1\nsetup\ftrouble\frequire\0", "config", "trouble.nvim")
+time([[Config for trouble.nvim]], false)
 -- Config for: gruvbox-material
 time([[Config for gruvbox-material]], true)
 try_loadstring("\27LJ\2\n@\0\0\3\0\3\0\0056\0\0\0009\0\1\0'\2\2\0B\0\2\1K\0\1\0!colorscheme gruvbox-material\bcmd\bvim\0", "config", "gruvbox-material")
